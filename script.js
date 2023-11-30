96c2c41b047bff268f92aa5948bcce86cf89304c
Misbahur rahman <98620184+Misbahrahman@users.noreply.github.com>
2023-11-30 21:22:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,24 +3,24 @@ const makeChange = (c) => {
 		q : 0,
 	n:0,
 	d:0,
-	p:0,
+	p:0
 	};
-  while(c > 25){
+  while(c >= 25){
 	  c-=25;
 	  obj.q++;
   }
 
-	while(c > 10){
+	while(c >= 10){
 		 c-=10;
 		obj.d++;
 	}
 
-	while(c > 5){
+	while(c >= 5){
 		c -= 5;
 		obj.n++;
 	}
 
-	while(c > 1){
+	while(c >= 1){
 		c-=1;
 		obj.p++;
 	}
